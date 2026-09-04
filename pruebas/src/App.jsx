d2be345c5cba08dca0be5daf837f86af7e91f625
joenhedav <235515282+joenhedav@users.noreply.github.com>
2026-09-04 12:58:42 -0300

    uso de la destructuración para las props

--- pruebas/src/App.jsx
+++ pruebas/src/App.jsx
@@ -1,6 +1,25 @@
-const App = () => {
+const Hello = ({ name, age }) => {
+  const bornYear = () => new Date().getFullYear() - age
+
   return (
     <div>
+      <p>
+        Hola {name}, you are {age} years old
+      </p>
+      <p>So you were probably born in {bornYear()}</p>
+    </div>
+  )
+}
+
+const App = () => {
+  const name = 'Peter'
+  const age = 5
+
+  return (
+    <div>
+      <h1>Greetings</h1>
+      <Hello name='Maya' age={3 + 1}/>
+      <Hello name={name} age={age}/>
     </div>
   )
 }
