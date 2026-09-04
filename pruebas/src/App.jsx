9ee043abc8459a9a24eab8c69d47d95da120794c
joenhedav <235515282+joenhedav@users.noreply.github.com>
2026-09-04 13:56:16 -0300

    cambiando el estado de los componentes
    uso del hook useState de React para cambiar el estado de los componentes

--- pruebas/src/App.jsx
+++ pruebas/src/App.jsx
@@ -1,25 +1,43 @@
-const Hello = ({ name, age }) => {
-  const bornYear = () => new Date().getFullYear() - age
+import { useState } from "react"
 
+const Display = ({counter}) => {
   return (
     <div>
-      <p>
-        Hola {name}, you are {age} years old
-      </p>
-      <p>So you were probably born in {bornYear()}</p>
+      {counter}
     </div>
   )
 }
 
+const Button = ({onClick, text}) => {
+  return (
+    <button onClick={onClick}>
+      {text}
+    </button>
+  )
+}
+
 const App = () => {
-  const name = 'Peter'
-  const age = 5
+  const [counter, setCounter] = useState(0)
+  
+  const increaseByOne = () => setCounter(counter + 1)
+  const decreaseByOne = () => setCounter(counter - 1)
+  const setToZero = () => setCounter(0)
 
   return (
     <div>
-      <h1>Greetings</h1>
-      <Hello name='Maya' age={3 + 1}/>
-      <Hello name={name} age={age}/>
+      <Display counter={counter}/>
+      <Button 
+        onClick={increaseByOne}
+        text='plus'
+      />
+      <Button 
+        onClick={setToZero}
+        text='zero'
+      />
+      <Button 
+        onClick={decreaseByOne}
+        text='minus'
+      />
     </div>
   )
 }
